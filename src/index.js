589fc69aa67da078c7bc641efbbc6ac28aa4e5e8
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-05-14 04:17:20 -0400

    refactor: change `App` path

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-
 import reactDom from 'react-dom/client';
 
-import App from './App';
+import App from './components/App';
 
 const root = reactDom.createRoot(document.getElementById('root'));
 
