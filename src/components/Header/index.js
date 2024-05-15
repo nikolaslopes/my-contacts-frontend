293ba80134d60a79ab6b58124cfc1c071b22629d
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-05-15 01:09:35 -0400

    refactor: remove `InputSearchContainer`

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,11 @@
-import { Container, InputSearchContainer } from './styles';
+import { Container } from './styles';
 
 import logo from '../../assets/images/logo.svg';
 
 export default function Header() {
   return (
     <Container>
-      <img src={logo} alt='MyContacts' width='200' />
-
-      <InputSearchContainer>
-        <input type='text' placeholder='Pesquisar contato' />
-      </InputSearchContainer>
+      <img src={logo} alt='MyContacts' width='201' />
     </Container>
   );
 }
