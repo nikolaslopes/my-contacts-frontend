3aa3b8a58e9c7644beaa6229208efeeafde5844a
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-05-14 23:24:29 -0400

    feat: create `ListContainer`

--- src/components/ContactsList/index.js
+++ src/components/ContactsList/index.js
@@ -1,4 +1,8 @@
-import { Container, Header } from './styles';
+import { Container, Header, ListContainer, Card } from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export default function ContactList() {
   return (
@@ -7,6 +11,75 @@ export default function ContactList() {
         <strong>3 contatos</strong>
         <a href='/'>Novo contato</a>
       </Header>
+
+      <ListContainer>
+        <header>
+          <button type='button'>
+            <span>Nome</span>
+            <img src={arrow} alt='Arrow' />
+          </button>
+        </header>
+
+        <Card>
+          <div className='info'>
+            <div className='contact-name'>
+              <strong>Nikolas Lopes</strong>
+              <small>Instagram</small>
+            </div>
+            <span>nikolas@email.com</span>
+            <span>(41) 99999-9999</span>
+          </div>
+
+          <div className='actions'>
+            <a href='/'>
+              <img src={edit} alt='Edit' />
+            </a>
+            <button type='button'>
+              <img src={trash} alt='Delete' />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className='info'>
+            <div className='contact-name'>
+              <strong>Nikolas Lopes</strong>
+              <small>Instagram</small>
+            </div>
+            <span>nikolas@email.com</span>
+            <span>(41) 99999-9999</span>
+          </div>
+
+          <div className='actions'>
+            <a href='/'>
+              <img src={edit} alt='Edit' />
+            </a>
+            <button type='button'>
+              <img src={trash} alt='Delete' />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className='info'>
+            <div className='contact-name'>
+              <strong>Nikolas Lopes</strong>
+              <small>Instagram</small>
+            </div>
+            <span>nikolas@email.com</span>
+            <span>(41) 99999-9999</span>
+          </div>
+
+          <div className='actions'>
+            <a href='/'>
+              <img src={edit} alt='Edit' />
+            </a>
+            <button type='button'>
+              <img src={trash} alt='Delete' />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
     </Container>
   );
 }
