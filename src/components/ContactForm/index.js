2c35b9eb27de70085987c1187ee7237c6bc3b0e2
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-05-17 23:27:57 -0400

    refactor: add `useErrors` custom hook to set, remove and get errors

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 
 import isEmailValid from '../../utils/isEmailValid';
+import useErrors from '../../hooks/useErrors';
 
 import Button from '../../components/Button';
 import FormGroup from '../../components/FormGroup';
@@ -15,20 +16,16 @@ export default function ContactForm({ buttonLabel }) {
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [category, setCategory] = useState('');
-  const [errors, setErrors] = useState([]);
+
+  const { setError, removeError, getErrorMessagesByFieldName } = useErrors();
 
   function handleNameChange(event) {
     setName(event.target.value);
 
     if (!event.target.value) {
-      setErrors((prevState) => [
-        ...prevState,
-        { field: 'name', message: 'Nome é obrigatório.' },
-      ]);
+      setError({ field: 'name', message: 'Nome é obrigatório.' });
     } else {
-      setErrors((prevState) =>
-        prevState.filter((error) => error.field !== 'name'),
-      );
+      removeError('name');
     }
   }
 
@@ -36,29 +33,10 @@ export default function ContactForm({ buttonLabel }) {
     setEmail(event.target.value);
 
     if (event.target.value && !isEmailValid(event.target.value)) {
-      const errorAlreadyExists = errors.find(
-        (error) => error.field === 'email',
-      );
-
-      if (errorAlreadyExists) {
-        return;
-      }
-
-      setErrors((prevState) => [
-        ...prevState,
-        { field: 'email', message: 'E-mail inválido.' },
-      ]);
+      setError({ field: 'email', message: 'E-mail inválido.' });
     } else {
-      setErrors((prevState) =>
-        prevState.filter((error) => error.field !== 'email'),
-      );
+      removeError('email');
     }
-  }
-
-  function getErrorMessagesByFieldName(fieldName) {
-    const error = errors.find((error) => error.field === fieldName);
-
-    return error?.message;
   }
 
   function handleSubmit(event) {
