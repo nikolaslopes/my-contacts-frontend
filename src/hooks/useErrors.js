7c1671671fd25e24ca48abc8eb0390d81e8b1b02
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-05-17 23:23:30 -0400

    feat: create `setError` custom hook

---
+++ src/hooks/useErrors.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+export default function useErrors() {
+  const [errors, setErrors] = useState([]);
+
+  function setError({ field, message }) {
+    const errorAlreadyExists = errors.find((error) => error.field === field);
+
+    if (errorAlreadyExists) {
+      return;
+    }
+
+    setErrors((prevState) => [...prevState, { field, message }]);
+  }
+
+  function removeError(fieldName) {
+    setErrors((prevState) =>
+      prevState.filter((error) => error.field !== fieldName),
+    );
+  }
+
+  function getErrorMessagesByFieldName(fieldName) {
+    const error = errors.find((error) => error.field === fieldName);
+
+    return error?.message;
+  }
+
+  return { setError, removeError, getErrorMessagesByFieldName };
+}
