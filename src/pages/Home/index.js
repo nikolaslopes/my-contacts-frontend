7937cf1ee20debf6b56dd3d0a34cebcff2ad9449
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-05-20 23:54:38 -0400

    refactor: change `ListContainer` to `ListHeader`

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,7 +5,7 @@ import {
   Container,
   InputSearchContainer,
   Header,
-  ListContainer,
+  ListHeader,
   Card,
 } from './styles';
 
@@ -40,38 +40,34 @@ export default function Home() {
         <Link to='/new'>Novo contato</Link>
       </Header>
 
-      <ListContainer>
-        <header>
-          <button type='button'>
-            <span>Nome</span>
-            <img src={arrow} alt='Arrow' />
-          </button>
-        </header>
+      <ListHeader>
+        <button type='button'>
+          <span>Nome</span>
+          <img src={arrow} alt='Arrow' />
+        </button>
+      </ListHeader>
 
-        {contacts.map((contact) => (
-          <Card key={contact.id}>
-            <div className='info'>
-              <div className='contact-name'>
-                <strong>{contact.name}</strong>
-                {contact.category_name && (
-                  <small>{contact.category_name}</small>
-                )}
-              </div>
-              <span>{contact.email}</span>
-              <span>{contact.phone}</span>
+      {contacts.map((contact) => (
+        <Card key={contact.id}>
+          <div className='info'>
+            <div className='contact-name'>
+              <strong>{contact.name}</strong>
+              {contact.category_name && <small>{contact.category_name}</small>}
             </div>
+            <span>{contact.email}</span>
+            <span>{contact.phone}</span>
+          </div>
 
-            <div className='actions'>
-              <Link to={`/edit/${contact.id}`}>
-                <img src={edit} alt='Edit' />
-              </Link>
-              <button type='button'>
-                <img src={trash} alt='Delete' />
-              </button>
-            </div>
-          </Card>
-        ))}
-      </ListContainer>
+          <div className='actions'>
+            <Link to={`/edit/${contact.id}`}>
+              <img src={edit} alt='Edit' />
+            </Link>
+            <button type='button'>
+              <img src={trash} alt='Delete' />
+            </button>
+          </div>
+        </Card>
+      ))}
     </Container>
   );
 }
