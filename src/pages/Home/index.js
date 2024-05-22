07de7cf36c47f036d2d4bfa070ca54fe70d71e4a
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-05-21 23:56:14 -0400

    feat: create contacts search

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -16,6 +16,13 @@ import trash from '../../assets/images/icons/trash.svg';
 export default function Home() {
   const [contacts, setContacts] = useState([]);
   const [orderBy, setOrderBy] = useState('asc');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
+
+  console.log(filteredContacts);
 
   useEffect(() => {
     fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
@@ -27,6 +34,10 @@ export default function Home() {
       .catch((error) => console.log('error', error));
   }, [orderBy]);
 
+  function handleChangeSearchTerm(event) {
+    setSearchTerm(event.target.value);
+  }
+
   function handleToggleOrderBy() {
     setOrderBy((prevState) => (prevState === 'asc' ? 'desc' : 'asc'));
   }
@@ -34,25 +45,32 @@ export default function Home() {
   return (
     <Container>
       <InputSearchContainer>
-        <input type='text' placeholder='Pesquisar contato' />
+        <input
+          type='text'
+          placeholder='Pesquisar contato'
+          value={searchTerm}
+          onChange={handleChangeSearchTerm}
+        />
       </InputSearchContainer>
 
       <Header>
         <strong>
-          {contacts.length}
-          {contacts.length === 1 ? ' contato' : ' contatos'}
+          {filteredContacts.length}
+          {filteredContacts.length === 1 ? ' contato' : ' contatos'}
         </strong>
         <Link to='/new'>Novo contato</Link>
       </Header>
 
-      <ListHeader orderBy={orderBy}>
-        <button type='button' onClick={handleToggleOrderBy}>
-          <span>Nome</span>
-          <img src={arrow} alt='Arrow' />
-        </button>
-      </ListHeader>
+      {filteredContacts.length > 0 && (
+        <ListHeader orderBy={orderBy}>
+          <button type='button' onClick={handleToggleOrderBy}>
+            <span>Nome</span>
+            <img src={arrow} alt='Arrow' />
+          </button>
+        </ListHeader>
+      )}
 
-      {contacts.map((contact) => (
+      {filteredContacts.map((contact) => (
         <Card key={contact.id}>
           <div className='info'>
             <div className='contact-name'>
