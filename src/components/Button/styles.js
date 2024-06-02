55c894c60cbab34993e907b094aa2d0c26a1895c
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-06-02 17:38:22 -0400

    refactor: change `Button` implemetation to be more functional

---
+++ src/components/Button/styles.js
@@ -0,0 +1,44 @@
+import styled, { css } from 'styled-components';
+
+export const StyledButton = styled.button`
+  height: 52px;
+  padding: 0 16px;
+  color: #FFFFFF;
+  background: ${({ theme }) => theme.colors.primary.main};
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  border-radius: 4px;
+  transition: background 0.2s ease-in;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    background: ${({ theme }) => theme.colors.primary.light};
+  }
+
+  &:active {
+    background: ${({ theme }) => theme.colors.primary.dark};
+  }
+
+  &[disabled] {
+    background: #ccc;
+    cursor: default;
+  }
+
+  ${({ theme, danger }) =>
+    danger &&
+    css`
+    background: ${theme.colors.danger.main};
+
+    &:hover {
+      background: ${theme.colors.danger.light};
+    }
+
+    &:active {
+      background: ${theme.colors.danger.dark};
+    }
+  `}
+`;
