ad71fa81ae902919db96e00ada3e55ccefb89557
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-06-03 23:06:10 -0400

    feat: implements custom event to render toast

--- src/components/Toast/ToastMessage/index.js
+++ src/components/Toast/ToastMessage/index.js
@@ -5,11 +5,11 @@ import { Container } from './styles';
 import xCircleIcon from '../../../assets/images/icons/x-circle.svg';
 import checkCircleIcon from '../../../assets/images/icons/check-circle.svg';
 
-export default function ToastMessage({ text, variant = 'default' }) {
+export default function ToastMessage({ text, type = 'default' }) {
   return (
-    <Container variant={variant}>
-      {variant === 'danger' && <img src={xCircleIcon} alt='X' />}
-      {variant === 'success' && <img src={checkCircleIcon} alt='Check' />}
+    <Container variant={type}>
+      {type === 'danger' && <img src={xCircleIcon} alt='X' />}
+      {type === 'success' && <img src={checkCircleIcon} alt='Check' />}
       <strong>{text}</strong>
     </Container>
   );
@@ -17,5 +17,5 @@ export default function ToastMessage({ text, variant = 'default' }) {
 
 ToastMessage.propTypes = {
   text: PropTypes.string.isRequired,
-  variant: PropTypes.oneOf(['default', 'success', 'danger']),
+  type: PropTypes.oneOf(['default', 'success', 'danger']),
 };
