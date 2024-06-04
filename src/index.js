67003f15cd3c51a9b00c8309b9096ec835a4ec4b
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-06-04 14:57:44 -0400

    feat: create event manager

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import reactDom from 'react-dom/client';
 
 import App from './components/App';
 
+import './lib/EventManager';
 const root = reactDom.createRoot(document.getElementById('root'));
 
 root.render(
