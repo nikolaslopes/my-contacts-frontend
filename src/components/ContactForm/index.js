abd9094fd5294856f50dcbdee40ab2449292b0c5
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-06-05 23:42:32 -0400

    refactor: clean form fields when form is submitted

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -83,6 +83,11 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
     });
 
     setIsSubmitting(false);
+
+    setName('');
+    setEmail('');
+    setPhone('');
+    setCategoryId('');
   }
 
   return (
