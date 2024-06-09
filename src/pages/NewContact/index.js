b16f4f44ab12dd1ad4cc497005a5d1813ce5e0ee
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-06-09 03:27:08 -0400

    refactor: change `NewContact` to use react hooks as main pattern

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,30 +1,10 @@
-import { useRef } from 'react';
-import ContactsService from '../../services/ContactsService';
-import toast from '../../utils/toast';
+import { useNewContact } from './useNewContact';
 
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
 
 export default function NewContact() {
-  const contactFormRef = useRef(null);
-
-  async function handleSubmit(contact) {
-    try {
-      await ContactsService.createContact(contact);
-
-      contactFormRef.current.resetFields();
-
-      toast({
-        type: 'success',
-        text: 'Contato cadastrado com sucesso!',
-      });
-    } catch {
-      toast({
-        type: 'danger',
-        text: 'Ocorreu um erro ao cadastrar o contato!',
-      });
-    }
-  }
+  const { contactFormRef, handleSubmit } = useNewContact();
 
   return (
     <>
