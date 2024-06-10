62b74dfe5bb53ea27197f0b1d6031c2922c7c697
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-06-09 20:16:24 -0400

    feat: add animation whem modal open

--- src/components/Modal/styles.js
+++ src/components/Modal/styles.js
@@ -1,4 +1,24 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`;
+
+const scaleIn = keyframes`
+  from {
+    transform: scale(0);
+  }
+
+  to {
+    transform: scale(1);
+  }
+`;
 
 export const Overlay = styled.div`
   width: 100%;
@@ -11,6 +31,7 @@ export const Overlay = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  animation: ${fadeIn} 0.3s;
 `;
 
 export const Container = styled.div`
@@ -20,6 +41,7 @@ export const Container = styled.div`
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
   padding: 24px;
   border-radius: 4px;
+  animation: ${scaleIn} 0.3s;
 
   > h1 {
     font-size: 22px;
