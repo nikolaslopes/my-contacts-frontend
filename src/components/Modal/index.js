1c5b7990b9acebdacf69f46a4114b5fac1bda6ce
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-06-10 08:51:33 -0400

    refactor: abstract animation end to a custom hook

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,6 @@
-import { useEffect, useRef, useState } from 'react';
-
 import PropTypes from 'prop-types';
+
+import useAnimatedUnmount from '../../hooks/useAnimatedUnmount';
 
 import ReactPortal from '../ReactPortal';
 import Button from '../../components/Button';
@@ -18,34 +18,7 @@ export default function Modal({
   onCancel,
   onConfirm,
 }) {
-  const [shouldRender, setShouldRender] = useState(visible);
-
-  const overlayRef = useRef(null);
-
-  useEffect(() => {
-    const overlayRefElement = overlayRef.current;
-
-    if (visible) {
-      setShouldRender(true);
-    }
-
-    function handleAnimationEnd() {
-      setShouldRender(false);
-    }
-
-    if (!visible && overlayRefElement) {
-      overlayRefElement.addEventListener('animationend', handleAnimationEnd);
-    }
-
-    return () => {
-      if (overlayRefElement) {
-        overlayRefElement.removeEventListener(
-          'animationend',
-          handleAnimationEnd,
-        );
-      }
-    };
-  }, [visible]);
+  const { shouldRender, animatedElementRef } = useAnimatedUnmount(visible);
 
   if (!shouldRender) {
     return null;
@@ -53,7 +26,7 @@ export default function Modal({
 
   return (
     <ReactPortal containerId='modal-root'>
-      <Overlay $isLeaving={!visible} ref={overlayRef}>
+      <Overlay $isLeaving={!visible} ref={animatedElementRef}>
         <Container danger={danger}>
           <h1>{title}</h1>
 
