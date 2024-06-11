805df5561b5a600673160aadeaba3401742e6684
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-06-11 17:08:05 -0400

    feat: link to home when click on logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,14 @@
 import { Container } from './styles';
 
 import logo from '../../assets/images/logo.svg';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <Container>
-      <img src={logo} alt='MyContacts' width='201' />
+      <Link to='/'>
+        <img src={logo} alt='MyContacts' width='201' />
+      </Link>
     </Container>
   );
 }
