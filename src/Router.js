1f5ab5cd8e0188d027a723518c46969144590795
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-06-11 17:08:35 -0400

    feat: animate routes

--- src/Router.js
+++ src/Router.js
@@ -1,25 +1,17 @@
-import { Routes, Route, useLocation } from 'react-router-dom';
-import { animated, useTransition } from 'react-spring';
+import useAnimatedRoutes from './hooks/useAnimatedRoute';
 
 import Home from './pages/Home';
 import NewContact from './pages/NewContact';
 import EditContact from './pages/EditContact';
 
 export default function Router() {
-  const location = useLocation();
-  const transitions = useTransition(location, {
-    from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
-    enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
-    leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
-  });
+  const routes = [
+    { path: '/', element: <Home /> },
+    { path: '/new', element: <NewContact /> },
+    { path: '/edit/:id', element: <EditContact /> },
+  ];
 
-  return transitions((props, item) => (
-    <animated.div key={item.key} style={props}>
-      <Routes location={item}>
-        <Route path='/' element={<Home />} />
-        <Route path='/new' element={<NewContact />} />
-        <Route path='/edit/:id' element={<EditContact />} />
-      </Routes>
-    </animated.div>
-  ));
+  const animatedRoutes = useAnimatedRoutes(routes);
+
+  return <>{animatedRoutes}</>;
 }
